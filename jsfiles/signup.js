ae02b87b8d57e8c01369276f45f4c7b6f4bd827f
Stavan Bhagat <bhagatstavan7@gmail.com>
2024-02-22 17:00:49 +0530

    creating master structure

---
+++ jsfiles/signup.js
@@ -0,0 +1,83 @@
+document.getElementById("myForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (validate()) {
+    const success = document.querySelector("#successMessage");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const city = document.getElementById("city").value;
+    const password = document.getElementById("password").value;
+
+    success.style.display = "block";
+    const data = {
+      name: name,
+      email: email,
+      city: city,
+      password: password,
+    };
+    console.log("success");
+    console.log(data);
+
+    var userdata = localStorage.setItem("users", JSON.stringify(data));
+    console.log(userdata);
+  }
+});
+
+function validate() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const city = document.getElementById("city").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  let valid = true;
+
+  const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+  if (name === "") {
+    document.getElementById("sname").innerHTML = "it should not be empty";
+    valid = false;
+  } else {
+    document.getElementById("sname").innerHTML = "";
+  }
+
+  if (email === "") {
+    document.getElementById("semail").innerHTML = "it should not be empty";
+    valid = false;
+  } else if (!emailRegex.test(email)) {
+    document.getElementById("semail").innerHTML = "incorrect email";
+    valid = false;
+  } else {
+    document.getElementById("semail").innerHTML = "";
+  }
+
+  if (city === "") {
+    document.getElementById("scity").innerHTML = "it should not be empty";
+    valid = false;
+  } else {
+    document.getElementById("scity").innerHTML = "";
+  }
+
+  if (password.length < 8) {
+    document.getElementById("spassword").innerHTML =
+      "it should contain 8 characters";
+    valid = false;
+  } else if (password === "") {
+    document.getElementById("spassword").innerHTML = "it should not be empty";
+    valid = false;
+  } else {
+    document.getElementById("spassword").innerHTML = "";
+  }
+
+  if (confirmPassword !== password) {
+    document.getElementById("sConfirmPassword").innerHTML =
+      "password does not match";
+    valid = false;
+  } else if (password === "") {
+    document.getElementById("sConfirmPassword").innerHTML =
+      "it should not be empty";
+    valid = false;
+  } else {
+    document.getElementById("sConfirmPassword").innerHTML = "";
+  }
+  return valid;
+}
