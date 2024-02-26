019386bb7932fea4bf45a73e579b9338e1b6e49b
Stavan <bhagatstavan7@gmail.com>
2024-02-26 06:27:52 +0530

    update functionality

--- jsfiles/welcomeadmin.js
+++ jsfiles/welcomeadmin.js
@@ -18,7 +18,10 @@ if (userdata.length > 0) {
     <td>${userdata[i].role}</td>
     <td>${userdata[i].city}</td>
    
-    <td><button type="button" class="btn btn-warning text-light" >Edit</button></td>
+    <td> <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#updateModal" onclick="updatedata('${userdata[i].email
+    }')">
+    Update
+  </button></td>
     <td><button type="button" class="btn btn-danger" onclick="deletedata('${userdata[i].email
       }')">Delete</button></td>
     </tr>`;
@@ -38,3 +41,40 @@ function deletedata(userEmail) {
   localStorage.setItem("users", JSON.stringify(restUsers));
   location.reload();
 }
+
+// update the data
+
+function updatedata(email) {
+  return email;
+}
+document.getElementById('submitBtn').addEventListener('click', function () {
+  let index = updatedata();
+  console.log(index);
+  let myform = document.forms['myform']
+  let uname = myform.elements['name'].value;
+  let uemail = myform.elements['email'].value;
+  let urole = myform.elements['role'].value;
+  let ucity = myform.elements['city'].value;
+
+  var updateddata = {
+    name: uname,
+    email: uemail,
+    role: urole,
+    city: ucity
+  }
+  var users = JSON.parse(localStorage.getItem("users"));
+  function updateUser(index, newData) {
+    if (users && users[index]) {
+
+      users[index] = newData;
+
+
+      localStorage.setItem('userData', JSON.stringify(users));
+      return true;
+    } else {
+      return false;
+    }
+  }
+  updateUser(updatedata, updateddata);
+
+});
